02825b5491efcbd8dccc74b6133a774e60945524
NILBOGtheSavior <naztechmedia@gmail.com>
2024-07-31 02:59:09 -0500

    slideshow updated

--- js/slideshow.js
+++ js/slideshow.js
@@ -2,10 +2,11 @@ document.addEventListener("DOMContentLoaded", (event) => {
     let text = document.getElementById('slideshow_text');
     let image = document.getElementById('slideshow_image');
     let i = 0;
+    let j = 0;
 
     var slideshow = [
         ['template1.png', 'template2.png'],
-        ['sleek, simple design', 'another description']
+        ['discover the simplicity and effectiveness of our clothing, a perfect blend of comfort and style', 'embrace effortless style with our clothing and accessories']
     ];
 
     function setSlide() {
@@ -26,7 +27,23 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
 
     function setText(reference) {
-        text.innerText = reference;
+        text.innerText = ""; // Clear previous text
+        j = 0; // Reset j for the new text
+        typeWriter(reference);
+    }
+
+    function typeWriter(reference) {
+        if (j < reference.length) {
+            let char = reference.charAt(j);
+            let renderTime = 2000 / reference.length;
+            if (char === ' ') {
+                text.innerHTML += '&emsp;' + char;
+            } else {
+                text.innerText += char;
+            }
+            j++;
+            setTimeout(() => typeWriter(reference), renderTime); // Adjust typing speed as needed
+        }
     }
 
     setSlide();
